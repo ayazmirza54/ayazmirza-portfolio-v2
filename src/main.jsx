b85d4140f224d81a2e4b61aabc5ca1a4d07a940a
Ayaz Mirza <37616943+ayazmirza54@users.noreply.github.com>
2024-12-04 15:17:43 +0000

    try another posthog fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { inject } from '@vercel/analytics';
-import { PostHogProvider} from 'posthog-js/react'
-import { BrowserRouter } from 'react-router-dom';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { inject } from "@vercel/analytics";
+import { BrowserRouter } from "react-router-dom";
+import posthog from "posthog-js";
 inject();
 
-const options = {
-  api_host: process.env.POSTHOG_HOST,
-}
+posthog.init("phc_gnfbhtpxbAZx2sKacxEAwEw49aGMIdmgdmzcFkTYhGG", {
+  api_host: "https://us.i.posthog.com",
+  person_profiles: "identified_only",
+});
 
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-     <BrowserRouter>
-     <PostHogProvider 
-      apiKey={process.env.POSTHOG_KEY}
-      options={options}
-    >
+    <BrowserRouter>
       <App />
-    </PostHogProvider>
     </BrowserRouter>
-  </StrictMode>,
-)
-
+  </StrictMode>
+);
