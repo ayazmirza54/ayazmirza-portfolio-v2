39410f7bc5c30a969b8223e0f0639bdb10397e2f
Ayaz Mirza <37616943+ayazmirza54@users.noreply.github.com>
2024-12-04 14:25:35 +0000

    Added posthog analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { inject } from '@vercel/analytics';
- 
+import posthog from 'posthog-js'
 inject();
+
+
+posthog.init('phc_gnfbhtpxbAZx2sKacxEAwEw49aGMIdmgdmzcFkTYhGG',
+    {
+        api_host: 'https://us.i.posthog.com',
+        person_profiles: 'identified_only' // or 'always' to create profiles for anonymous users as well
+    }
+)
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
