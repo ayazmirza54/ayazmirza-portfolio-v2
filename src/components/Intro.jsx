1bbf0559b298335260431f50186edcd84d6abdae
Ayaz Mirza <37616943+ayazmirza54@users.noreply.github.com>
2025-06-19 22:32:50 +0530

    Update Intro.jsx

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -6,20 +6,7 @@ const Intro = () => {
       <div className="text-2xl md:text-4xl font-extrabold capitalize"><h1>Introduction</h1></div>
       <br></br>
       <p>
-        Experienced in using Ab Initio to create strong data processing
-        workflows, and skilled at writing shell scripts to keep things running
-        smoothly. Good at writing SQL queries that make databases run faster and
-        more efficiently. Proficient with Power BI, creating clear, visual
-        reports and dashboards that help make decisions based on data.
-        <br></br>
-        Also a front-end developer who enjoys building websites that are easy to
-        use and look good. Knowledgeable in HTML, CSS, and JavaScript, and pays
-        close attention to design and accessibility to ensure everyone can use
-        the site. Committed to learning and keeping up with new trends in web
-        development.
-        <br></br>
-        Proficient in using AI tools like Gemini AI, Claude AI, and OpenAI
-        ChatGPT.
+        I’m a Senior Consultant at Wipro with 5 years of experience in the telecom sector, currently supporting a major Indonesian telecom provider. I build efficient data processing systems, automate tasks using shell and Python scripts, and manage servers with automation tools like Ansible. Additionally, I have a strong background in web development using ReactJS and ExpressJS. I regularly use AI tools such as Gemini, Claude, and ChatGPT to simplify workflows. My goal is always to deliver simple, reliable, and practical solutions.
       </p>
     </div>
   );
